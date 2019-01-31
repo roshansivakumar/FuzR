1e251eacd778a87a56b9da940d1dceceb81eb474
Roshan Sivakumar <roshan.sivakumar001@gmail.com>
2019-01-31 10:50:04 +0530

    update - trapmf and trimf complete

---
+++ fuzzR.py
@@ -0,0 +1,495 @@
+from mfArea import mfAreaCertainity as mfAC
+from mfArea import mfAreaUncertainity as mfAU
+import numpy as np
+import matplotlib.pyplot as plt
+import skfuzzy as fuzz
+import pandas as pd
+from scipy.stats import norm
+# data = pd.read_excel("data.xlsx")
+"""
+Distribution 1 [0-100]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    28       |
+Interval2 |    26       |    40       |
+Interval3 |    36       |    52       |
+Interval4 |    44       |    71       |
+Interval5 |    63       |    94       |
+
+Distribution 2 [0-10]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    1        |
+Interval2 |    0.5      |    3.2      |
+Interval3 |    2.4      |    6        |
+Interval4 |    5        |    10       |
+"""
+dist1 = np.arange(0, 101, .01)
+dist2 = np.arange(0, 11, .01)
+
+# ***********************TRIANGULAR MF'S - Distribution 1*****************************
+mft1d1 = fuzz.trimf(dist1, [0.0, 14.0, 28.0])
+mft2d1 = fuzz.trimf(dist1, [26.0, 33.0, 40.0])
+mft3d1 = fuzz.trimf(dist1, [36.0, 44.0, 52.0])
+mft4d1 = fuzz.trimf(dist1, [44.0, 57.5, 71.0])
+mft5d1 = fuzz.trimf(dist1, [63.0, 78.5, 94.0])
+
+# ***********************TRIANGULAR MF'S - Distribution 2*****************************
+mft1d2 = fuzz.trimf(dist2, [0.0, 0.5, 1.0])
+mft2d2 = fuzz.trimf(dist2, [0.5, 1.85, 3.2])
+mft3d2 = fuzz.trimf(dist2, [2.4, 4.2, 6.0])
+mft4d2 = fuzz.trimf(dist2, [5.0, 7.5, 10.0])
+
+fig, (ax0, ax1, ax4, ax5) = plt.subplots(nrows=4, figsize=(7, 10))
+ax0.plot(dist1, mft1d1, 'b', linewidth=1)
+ax0.plot(dist1, mft2d1, 'b', linewidth=1)
+ax0.plot(dist1, mft3d1, 'b', linewidth=1)
+ax0.plot(dist1, mft4d1, 'b', linewidth=1)
+ax0.plot(dist1, mft5d1, 'b', linewidth=1)
+
+ax1.plot(dist2, mft1d2, 'g', linewidth=1)
+ax1.plot(dist2, mft2d2, 'g', linewidth=1)
+ax1.plot(dist2, mft3d2, 'g', linewidth=1)
+ax1.plot(dist2, mft4d2, 'g', linewidth=1)
+
+# Area of Uncertainity - Dist1
+a1tUd1 = mfAU.trimfAreaU(dist1, [0.0, 14.0, 28.0], [26.0, 33.0, 40.0])
+a2tUd1 = mfAU.trimfAreaU(dist1, [26.0, 33.0, 40.0], [36.0, 44.0, 52.0])
+a3tUd1 = mfAU.trimfAreaU(dist1, [36.0, 44.0, 52.0], [44.0, 57.5, 71.0])
+a4tUd1 = mfAU.trimfAreaU(dist1, [44.0, 57.5, 71.0], [63.0, 78.5, 94.0])
+
+# Area of Uncertainity - Dist2
+a1tUd2 = mfAU.trimfAreaU(dist2, [0.0, 0.5, 1.0], [0.5, 1.85, 3.2])
+a2tUd2 = mfAU.trimfAreaU(dist2, [0.5, 1.85, 3.2], [2.4, 4.2, 6.0])
+a3tUd2 = mfAU.trimfAreaU(dist2, [2.4, 4.2, 6.0], [5.0, 7.5, 10.0])
+print("\n****************UNCERTAINITY - Distribution 1 - Triangular MF's**************************")
+print("AreaU 1 = ")
+print(a1tUd1)
+print("AreaU 2 = ")
+print(a2tUd1)
+print("AreaU 3 = ")
+print(a3tUd1)
+print("AreaU 4 = ")
+print(a4tUd1)
+print("Total Area of Uncertainity =")
+totAreaUd1 = a1tUd1 + a2tUd1 + a3tUd1 + a4tUd1
+print(totAreaUd1)
+
+print("\n****************UNCERTAINITY - Distribution 2 - Triangular MF's**************************")
+print("AreaU 1 = ")
+print(a1tUd2)
+print("AreaU 2 = ")
+print(a2tUd2)
+print("AreaU 3 = ")
+print(a3tUd2)
+print("Total Area of Uncertainity =")
+totAreaUd2 = a1tUd2 + a2tUd2 + a3tUd2
+print(totAreaUd2)
+
+# Area of Certainity - Dist1
+a1tCd1 = mfAC.trimfAreaC(dist1, [0.0, 14.0, 28.0], [26.0, 33.0, 40.0], 0, a1tUd1)
+a2tCd1 = mfAC.trimfAreaC(dist1, [26.0, 33.0, 40.0], [36.0, 44.0, 52.0], a1tUd1, a2tUd1)
+a3tCd1 = mfAC.trimfAreaC(dist1, [36.0, 44.0, 52.0], [44.0, 57.5, 71.0], a2tUd1, a3tUd1)
+a4tCd1 = mfAC.trimfAreaC(dist1, [44.0, 57.5, 71.0], [63.0, 78.5, 94.0], a3tUd1, a4tUd1)
+a5tCd1 = mfAC.trimfAreaC(dist1, [63.0, 78.5, 94.0], [0, 0, 0], a4tUd1, 0)
+
+# Area of Certainity - Dist2
+a1tCd2 = mfAC.trimfAreaC(dist2, [0.0, 0.5, 1.0], [0.5, 1.85, 3.2], 0, a1tUd2)
+a2tCd2 = mfAC.trimfAreaC(dist2, [0.5, 1.85, 3.2], [2.4, 4.2, 6.0], a1tUd2, a2tUd2)
+a3tCd2 = mfAC.trimfAreaC(dist2, [2.4, 4.2, 6.0], [5.0, 7.5, 10.0], a2tUd2, a3tUd2)
+a4tCd2 = mfAC.trimfAreaC(dist2, [5.0, 7.5, 10.0], [0, 0, 0], a3tUd1, 0)
+print("******************CERTAINITY - Distribution 1 - Triangular MF's**************************")
+print("AreaU 1 = ")
+print(a1tCd1)
+print("AreaU 2 = ")
+print(a2tCd1)
+print("AreaU 3 = ")
+print(a3tCd1)
+print("AreaU 4 = ")
+print(a4tCd1)
+print("AreaU 5 = ")
+print(a5tCd1)
+print("Total Area of Certainity =")
+totAreaCd1 = a1tCd1 + a2tCd1 + a3tCd1 + a4tCd1 + a5tCd1
+print(totAreaCd1)
+
+print("******************CERTAINITY - Distribution 2 - Triangular MF's**************************")
+print("AreaU 1 = ")
+print(a1tCd2)
+print("AreaU 2 = ")
+print(a2tCd2)
+print("AreaU 3 = ")
+print(a3tCd2)
+print("AreaU 4 = ")
+print(a4tCd2)
+print("Total Area of Certainity =")
+totAreaCd2 = a1tCd2 + a2tCd2 + a3tCd2 + a4tCd2
+print(totAreaCd2)
+
+print("\nfactor for TRIANGULAR MEMBERSHIP FUNCTION- Distribution 1")
+print(totAreaCd1/totAreaUd1)
+
+print("\nfactor for TRIANGULAR MEMBERSHIP FUNCTION- Distribution 2")
+print(totAreaCd2/totAreaUd2)
+
+# ***********************GAUSSIAN MF'S****************************
+"""
+Distribution 1 [0-100]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    28       |
+Interval2 |    26       |    40       |
+Interval3 |    36       |    52       |
+Interval4 |    44       |    71       |
+Interval5 |    63       |    94       |
+
+Distribution 2 [0-10]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    1        |
+Interval2 |    0.5      |    3.2      |
+Interval3 |    2.4      |    6        |
+Interval4 |    5        |    10       |
+"""
+"""
+# ***********************GAUSSIAN MF'S - Distribution 1*****************************
+mfg1d1 =fuzz.gaussmf(dist1, 14, 65.3)
+mfg2d1 = fuzz.gaussmf(dist1, 33, 16.3)
+mfg3d1 = fuzz.gaussmf(dist1, 44, 21.3)
+mfg4d1 = fuzz.gaussmf(dist1, 57.5, 24.1)
+mfg5d1 = fuzz.gaussmf(dist1, 78.5, 36.7)
+
+# ***********************GAUSSIAN MF'S - Distribution 2*****************************
+mfg1d2 = fuzz.gaussmf(dist2, 0.5, 0.083)
+mfg2d2 = fuzz.gaussmf(dist2, 1.85, 0.6)
+mfg3d2 = fuzz.gaussmf(dist2, 4.2, 1.08)
+mfg4d2 = fuzz.gaussmf(dist2, 7.5, 2.08)
+
+ax2.plot(dist1, mfg1d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg2d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg3d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg4d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg5d1, 'r', linewidth=1)
+
+ax3.plot(dist2, mfg1d2, 'r', linewidth=1)
+ax3.plot(dist2, mfg2d2, 'r', linewidth=1)
+ax3.plot(dist2, mfg3d2, 'r', linewidth=1)
+ax3.plot(dist2, mfg4d2, 'r', linewidth=1)
+
+# Area of Uncertainity - Dist1
+a1gUd1 = mfAU.gaussmfAreaU(dist1, [14, 65.3], [33, 16.3])
+a2gUd1 = mfAU.gaussmfAreaU(dist1, [33, 16.3], [44, 21.3])
+a3gUd1 = mfAU.gaussmfAreaU(dist1, [44, 21.3], [57.5, 24.1])
+a4gUd1 = mfAU.gaussmfAreaU(dist1, [57.5, 24.1], [78.5, 36.7])
+
+# Area of Uncertainity - Dist2
+a1gUd2 = mfAU.gaussmfAreaU(dist2, [0.5, 0.083], [1.85, 0.6])
+a2gUd2 = mfAU.gaussmfAreaU(dist2, [1.85, 0.6], [4.2, 1.08])
+a3gUd2 = mfAU.gaussmfAreaU(dist2, [4.2, 1.08], [7.5, 2.08])
+print("\n****************UNCERTAINITY - Distribution 1 - GAUSSIAN MF's**************************")
+print("AreaU 1 = ")
+print(a1gUd1)
+print("AreaU 2 = ")
+print(a2gUd1)
+print("AreaU 3 = ")
+print(a3gUd1)
+print("AreaU 4 = ")
+print(a4gUd1)
+print("Total Area of Uncertainity =")
+totAreagUd1 = a1gUd1 + a2gUd1 + a3gUd1 + a4gUd1
+print(totAreagUd1)
+
+print("\n****************UNCERTAINITY - Distribution 2 - GAUSSIAN MF's**************************")
+print("AreaU 1 = ")
+print(a1gUd2)
+print("AreaU 2 = ")
+print(a2gUd2)
+print("AreaU 3 = ")
+print(a3gUd2)
+print("Total Area of Uncertainity =")
+totAreagUd2 = a1gUd2 + a2gUd2 + a3gUd2
+print(totAreagUd2)
+
+# Area of Certainity - Dist1
+a1gCd1 = mfAC.gaussmfAreaC(dist1, [14, 65.3], [33, 16.3], 0, a1gUd1)
+a2gCd1 = mfAC.gaussmfAreaC(dist1, [33, 16.3], [44, 21.3], a1gUd1, a2gUd1)
+a3gCd1 = mfAC.gaussmfAreaC(dist1, [44, 21.3], [57.5, 24.1], a2gUd1, a3gUd1)
+a4gCd1 = mfAC.gaussmfAreaC(dist1, [57.5, 24.1], [78.5, 36.7], a3gUd1, a4gUd1)
+a5gCd1 = mfAC.gaussmfAreaC(dist1, [78.5, 36.7], [0, 0], a4gUd1, 0)
+
+# Area of Certainity - Dist2
+a1gCd2 = mfAC.gaussmfAreaC(dist2, [0.5, 0.083], [1.85, 0.6], 0, a1gUd2)
+a2gCd2 = mfAC.gaussmfAreaC(dist2, [1.85, 0.6], [4.2, 1.08], a1gUd2, a2gUd2)
+a3gCd2 = mfAC.gaussmfAreaC(dist2, [4.2, 1.08], [7.5, 2.08], a2gUd2, a3gUd2)
+a4gCd2 = mfAC.gaussmfAreaC(dist2, [7.5, 2.08], [0, 0], a3gUd2, 0)
+print("******************CERTAINITY - Distribution 1 - GAUSSIAN MF's**************************")
+print("AreaU 1 = ")
+print(a1gCd1)
+print("AreaU 2 = ")
+print(a2gCd1)
+print("AreaU 3 = ")
+print(a3gCd1)
+print("AreaU 4 = ")
+print(a4gCd1)
+print("AreaU 5 = ")
+print(a5gCd1)
+print("Total Area of Certainity =")
+totAreagCd1 = a1gCd1 + a2gCd1 + a3gCd1 + a4gCd1 + a5gCd1
+print(totAreagCd1)
+
+print("******************CERTAINITY - Distribution 2 - GAUSSIAN MF's**************************")
+print("AreaU 1 = ")
+print(a1gCd2)
+print("AreaU 2 = ")
+print(a2gCd2)
+print("AreaU 3 = ")
+print(a3gCd2)
+print("AreaU 4 = ")
+print(a4gCd2)
+print("Total Area of Certainity =")
+totAreagCd2 = a1gCd2 + a2gCd2 + a3gCd2 + a4gCd2
+print(totAreagCd2)
+
+print("\nfactor for GAUSSIAN MEMBERSHIP FUNCTION- Distribution 1")
+print(totAreagCd1/totAreagUd1)
+
+print("\nfactor for GAUSSIAN MEMBERSHIP FUNCTION- Distribution 2")
+print(totAreagCd2/totAreagUd2)
+
+# Show plot
+#plt.tight_layout()
+"""
+
+# ***********************TRAPEZOIDAL MF'S*****************************
+"""
+Distribution 1 [0-100]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    28       |
+Interval2 |    26       |    40       |
+Interval3 |    36       |    52       |
+Interval4 |    44       |    71       |
+Interval5 |    63       |    94       |
+
+Distribution 2 [0-10]
+--------------
+Intervals | Lower Limit | Upper Limit |
+Interval1 |    0        |    1        |
+Interval2 |    0.5      |    3.2      |
+Interval3 |    2.4      |    6        |
+Interval4 |    5        |    10       |
+"""
+# ***********************TRAPEZOIDAL MF'S - Distribution 1*********************
+mftp1d1 = fuzz.trapmf(dist1, [0.0, 7.0, 21.0, 28.0])
+mftp2d1 = fuzz.trapmf(dist1, [26.0, 29.5, 36.5, 40.0])
+mftp3d1 = fuzz.trapmf(dist1, [36.0, 40.0, 48.0, 52.0])
+mftp4d1 = fuzz.trapmf(dist1, [44.0, 50.7, 64.1, 71.0])
+mftp5d1 = fuzz.trapmf(dist1, [63.0, 70.7, 83.1, 94.0])
+print(len(dist2))
+# ***********************TRAPEZOIDAL MF'S - Distribution 2********************
+mftp1d2 = fuzz.trapmf(dist2, [0.0, 0.25, 0.75, 1.0])
+mftp2d2 = fuzz.trapmf(dist2, [0.5, 1.17, 2.51, 3.2])
+mftp3d2 = fuzz.trapmf(dist2, [2.4, 3.3, 5.1, 6])
+mftp4d2 = fuzz.trapmf(dist2, [5, 6.25, 8.75, 10])
+
+ax4.plot(dist1, mftp1d1, 'b', linewidth=1)
+ax4.plot(dist1, mftp2d1, 'b', linewidth=1)
+ax4.plot(dist1, mftp3d1, 'b', linewidth=1)
+ax4.plot(dist1, mftp4d1, 'b', linewidth=1)
+ax4.plot(dist1, mftp5d1, 'b', linewidth=1)
+
+ax5.plot(dist2, mftp1d2, 'g', linewidth=1)
+ax5.plot(dist2, mftp2d2, 'g', linewidth=1)
+ax5.plot(dist2, mftp3d2, 'g', linewidth=1)
+ax5.plot(dist2, mftp4d2, 'g', linewidth=1)
+
+# Area of Uncertainity - Dist1
+a1tpUd1 = mfAU.trapmfAreaU(dist1, [0.0, 7.0, 21.0, 28.0], [26.0, 29.5, 36.5, 40.0])
+a2tpUd1 = mfAU.trapmfAreaU(dist1, [26.0, 29.5, 36.5, 40.0], [36.0, 40.0, 48.0, 52.0])
+a3tpUd1 = mfAU.trapmfAreaU(dist1, [36.0, 40.0, 48.0, 52.0], [44.0, 50.7, 64.1, 71.0])
+a4tpUd1 = mfAU.trapmfAreaU(dist1, [44.0, 50.7, 64.1, 71.0], [63.0, 70.7, 83.1, 94.0])
+
+# Area of Uncertainity - Dist2
+a1tpUd2 = mfAU.trapmfAreaU(dist2, [0.0, 0.25, 0.75, 1.0], [0.5, 1.17, 2.51, 3.2])
+a2tpUd2 = mfAU.trapmfAreaU(dist2, [0.5, 1.17, 2.51, 3.2], [2.4, 3.3, 5.1, 6])
+a3tpUd2 = mfAU.trapmfAreaU(dist2, [2.4, 3.3, 5.1, 6], [5, 6.25, 8.75, 10])
+print("\n****************UNCERTAINITY - Distribution 1 - TRAPEZOIDAL MF's**************************")
+print("AreaU 1 = ")
+print(a1tpUd1)
+print("AreaU 2 = ")
+print(a2tpUd1)
+print("AreaU 3 = ")
+print(a3tpUd1)
+print("AreaU 4 = ")
+print(a4tpUd1)
+print("Total Area of Uncertainity =")
+totAreatpUd1 = a1tpUd1 + a2tpUd1 + a3tpUd1 + a4tpUd1
+print(totAreatpUd1)
+
+print("\n****************UNCERTAINITY - Distribution 2 - TRAPEZOIDAL MF's**************************")
+print("AreaU 1 = ")
+print(a1tpUd2)
+print("AreaU 2 = ")
+print(a2tpUd2)
+print("AreaU 3 = ")
+print(a3tpUd2)
+print("Total Area of Uncertainity =")
+totAreatpUd2 = a1tpUd2 + a2tpUd2 + a3tpUd2
+print(totAreatpUd2)
+
+# Area of Certainity - Dist1
+a1tpCd1 = mfAC.trapmfAreaC(dist1, [0.0, 7.0, 21.0, 28.0], [26.0, 29.5, 36.5, 40.0], 0, a1tpUd1)
+a2tpCd1 = mfAC.trapmfAreaC(dist1, [26.0, 29.5, 36.5, 40.0], [36.0, 40.0, 48.0, 52.0], a1tpUd1, a2tpUd1)
+a3tpCd1 = mfAC.trapmfAreaC(dist1, [36.0, 40.0, 48.0, 52.0], [44.0, 50.7, 64.1, 71.0], a2tpUd1, a3tpUd1)
+a4tpCd1 = mfAC.trapmfAreaC(dist1, [44.0, 50.7, 64.1, 71.0], [63.0, 70.7, 83.1, 94.0], a3tpUd1, a4tpUd1)
+a5tpCd1 = mfAC.trapmfAreaC(dist1, [63.0, 70.7, 83.1, 94.0], [0, 0, 0, 0], a4tpUd1, 0)
+
+# Area of Certainity - Dist2
+a1tpCd2 = mfAC.trapmfAreaC(dist2, [0.0, 0.25, 0.75, 1.0], [0.5, 1.17, 2.51, 3.2], 0, a1tpUd2)
+a2tpCd2 = mfAC.trapmfAreaC(dist2, [0.5, 1.17, 2.51, 3.2], [2.4, 3.3, 5.1, 6], a1tpUd2, a2tpUd2)
+a3tpCd2 = mfAC.trapmfAreaC(dist2, [2.4, 3.3, 5.1, 6], [5, 6.25, 8.75, 10], a2tpUd2, a3tpUd2)
+a4tpCd2 = mfAC.trapmfAreaC(dist2, [5, 6.25, 8.75, 10], [0, 0, 0, 0], a3tpUd2, 0)
+print("******************CERTAINITY - Distribution 1 - TRAPEZOIDAL MF's**************************")
+print("AreaU 1 = ")
+print(a1tpCd1)
+print("AreaU 2 = ")
+print(a2tpCd1)
+print("AreaU 3 = ")
+print(a3tpCd1)
+print("AreaU 4 = ")
+print(a4tpCd1)
+print("AreaU 5 = ")
+print(a5tpCd1)
+print("Total Area of Certainity =")
+totAreatpCd1 = a1tpCd1 + a2tpCd1 + a3tpCd1 + a4tpCd1 + a5tpCd1
+print(totAreatpCd1)
+
+print("******************CERTAINITY - Distribution 2 - TRAPEZOIDAL MF's**************************")
+print("AreaU 1 = ")
+print(a1tpCd2)
+print("AreaU 2 = ")
+print(a2tpCd2)
+print("AreaU 3 = ")
+print(a3tpCd2)
+print("AreaU 4 = ")
+print(a4tpCd2)
+print("Total Area of Certainity =")
+totAreatpCd2 = a1tpCd2 + a2tpCd2 + a3tpCd2 + a4tpCd2
+print(totAreatpCd2)
+
+print("\nfactor for TRAPEZOIDAL MEMBERSHIP FUNCTION- Distribution 1")
+print(totAreatpCd1/totAreatpUd1)
+
+print("\nfactor for TRAPEZOIDAL MEMBERSHIP FUNCTION- Distribution 2")
+print(totAreatpCd2/totAreatpUd2)
+
+plt.show()
+
+
+"""
+#*********************TEST AND VALIDATION*****************************
+print("                 test                ")
+dist1Fig3 = np.arange(0, 10, .01)
+mf1test = fuzz.trimf(dist1, [0.0, 1.1, 3.0])
+mf2test = fuzz.trimf(dist1, [1.0, 4.8, 7])
+mf3test = fuzz.trimf(dist1, [6.0, 7.1, 8.0])
+
+
+# Area of Uncertainity
+a1tUtest = mfAU.trimfAreaU(dist1Fig3, [0.0, 1.1, 3.0], [1.0, 4.8, 7])
+a2tUtest = mfAU.trimfAreaU(dist1Fig3, [1.0, 4.8, 7], [6.0, 7.1, 8.0])
+print("\n****************UNCERTAINITY**************************")
+print("AreaU 1 = ")
+print(a1tUtest)
+print("AreaU 2 = ")
+print(a2tUtest)
+print("Total Area of Uncertainity =")
+totAreaUtest = a1tUtest + a2tUtest
+print(totAreaUtest)
+
+# Area of Certainity
+a1tCtest = mfAC.trimfAreaC(dist1Fig3, [0.0, 1.1, 3.0], [1.0, 4.8, 7], 0, a1tUtest)
+a2tCtest = mfAC.trimfAreaC(dist1Fig3, [1.0, 4.8, 7], [6.0, 7.1, 8.0], a1tUtest, a2tUtest)
+a3tCtest = mfAC.trimfAreaC(dist1Fig3, [6.0, 7.1, 8.0], [0, 0, 0], a2tUtest, 0)
+print("******************CERTAINITY**************************")
+print("AreaU 1 = ")
+print(a1tCtest)
+print("AreaU 2 = ")
+print(a2tCtest)
+print("AreaU 3 = ")
+print(a3tCtest)
+print("Total Area of Certainity =")
+totAreaCtest = a1tCtest + a2tCtest + a3tCtest
+print(totAreaCtest)
+
+print("\nTEST factor for TRIANGULAR MEMBERSHIP FUNCTION")
+print(totAreaCtest/totAreaUtest)
+
+# TEST
+print("##################################TEST&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+M1 = fuzz.trapmf(dist1, [0.0, 5.0, 15.0, 20.0])
+M2 = fuzz.trapmf(dist1, [18.0, 21.5, 28.5, 32.0])
+M3 = fuzz.trapmf(dist1, [28.0, 32.0, 40.0, 44.0])
+M4 = fuzz.trapmf(dist1, [39.0, 45.0, 57.0, 63.0])
+M5 = fuzz.trapmf(dist1, [60.0, 66.25, 78.75, 85.0])
+
+# ***********************TRAPEZOIDAL MF'S - Distribution 2********************
+
+m1 = fuzz.trapmf(dist2, [0.0, 0.25, 0.75, 1.0])
+m2 = fuzz.trapmf(dist2, [0.4, 1.05, 2.35, 3.0])
+m3 = fuzz.trapmf(dist2, [1.0, 2.53, 5.6, 7.14])
+m4 = fuzz.trapmf(dist2, [3.0, 4.75, 8.25, 10])
+--------test on graph-------
+m1 = fuzz.trapmf(dist2, [0.0, 1.0, 3.0, 4.0])
+m2 = fuzz.trapmf(dist2, [2.0, 4.0, 6.0, 7.0])
+
+ax4.plot(dist1, M1, 'b', linewidth=1)
+ax4.plot(dist1, M2, 'b', linewidth=1)
+ax4.plot(dist1, M3, 'b', linewidth=1)
+ax4.plot(dist1, M4, 'b', linewidth=1)
+ax4.plot(dist1, M5, 'b', linewidth=1)
+
+ax5.plot(dist2, m1, 'b', linewidth=1)
+ax5.plot(dist2, m2, 'b', linewidth=1)
+--------------test on graph------
+ax5.plot(dist2, m3, 'b', linewidth=1)
+ax5.plot(dist2, m4, 'b', linewidth=1)
+
+
+a1tpUd1 = mfAU.trapmfAreaU(dist1, [0.0, 5.0, 15.0, 20.0], [18.0, 21.5, 28.5, 32.0])
+a2tpUd1 = mfAU.trapmfAreaU(dist1,[18.0, 21.5, 28.5, 32.0], [28.0, 32.0, 40.0, 44.0])
+a3tpUd1 = mfAU.trapmfAreaU(dist1, [28.0, 32.0, 40.0, 44.0], [39.0, 45.0, 57.0, 63.0])
+a4tpUd1 = mfAU.trapmfAreaU(dist1, [39.0, 45.0, 57.0, 63.0], [60.0, 66.25, 78.75, 85.0])
+
+# Area of Uncertainity - Dist2
+
+a1tpUd2 = mfAU.trapmfAreaU(dist2, [0.0, 0.25, 0.75, 1.0], [0.4, 1.05, 2.35, 3.0])
+a2tpUd2 = mfAU.trapmfAreaU(dist2, [0.4, 1.05, 2.35, 3.0], [1.0, 2.53, 5.6, 7.14])
+a3tpUd2 = mfAU.trapmfAreaU(dist2, [1.0, 2.53, 5.6, 7.14], [3.0, 4.75, 8.25, 10])
+------------test on graph-----------
+a1tpUd2 = mfAU.trapmfAreaU(dist2, [0.0, 1.0, 3.0, 4.0], [2.0, 4.0, 6.0, 7.0])
+
+print("\n****************UNCERTAINITY - Distribution 1 - TRAPEZOIDAL MF's **************************")
+print("AreaU 1 = ")
+print(a1tpUd1)
+print("AreaU 2 = ")
+print(a2tpUd1)
+print("AreaU 3 = ")
+print(a3tpUd1)
+print("AreaU 4 = ")
+print(a4tpUd1)
+print("Total Area of Uncertainity =")
+totAreatpUd1 = a1tpUd1 + a2tpUd1 + a3tpUd1 + a4tpUd1
+print(totAreatpUd1)
+
+print("\n****************UNCERTAINITY - Distribution 2 - TRAPEZOIDAL MF's**************************")
+print("AreaU 1 = ")
+print(a1tpUd2)
+print("AreaU 2 = ")
+print(a2tpUd2)
+print("AreaU 3 = ")
+print(a3tpUd2)
+print("Total Area of Uncertainity =")
+totAreatpUd2 = a1tpUd2 + a2tpUd2 + a3tpUd3
+print(totAreatpUd2)
+"""
---
+++ mfArea.py
@@ -0,0 +1,139 @@
+"""
+Class that contains functions to obtain both the cetainity and uncertainity reigon
+"""
+import numpy as np
+import warnings
+from scipy.stats import norm
+import scipy.integrate as integrate
+
+class mfAreaUncertainity:
+
+    def trimfAreaU(x, params1, params2):
+
+        assert len(params1) == 3, 'abc parameter must have exactly three elements.'
+        assert len(params2) == 3, 'fgh parameter must have exactly three elements.'
+        a, b, c = np.r_[params1]     # Zero-indexing in Python
+        f, g, h = np.r_[params2]     # Zero-indexing in Python
+        assert a <= b and b <= c, 'abc requires the three elements a <= b <= c.'
+        assert f <= g and g <= h, 'fgh requires the three elements a <= b <= c.'
+        # Point of Intersection
+        x1 = b
+        y1 = 1
+        x2 = c
+        y2 = 0
+        x3 = f
+        y3 = 0
+        x4 = g
+        y4 = 1
+
+        try: # find parameters that make denominator zero
+            px = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+            py = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+        except RuntimeWarning: # Used since, I was getting / 0 runtimeWarning
+            print(a,b,c,f,g,h)
+        pt = [px, py]
+
+        # Area
+        area = 0.5*py*(c-f)
+        """
+        Alternative - using Clamers rule
+        def line(p1, p2):
+            A = (p1[1] - p2[1])
+            B = (p2[0] - p1[0])
+            C = (p1[0]*p2[1] - p2[0]*p1[1])
+            return A, B, -C
+
+        def intersection(L1, L2):
+            D  = L1[0] * L2[1] - L1[1] * L2[0]
+            Dx = L1[2] * L2[1] - L1[1] * L2[2]
+            Dy = L1[0] * L2[2] - L1[2] * L2[0]
+            if D != 0:
+                x = Dx / D
+                y = Dy / D
+                return x,y
+        L1 = line([4,1], [6,0])
+        L2 = line([5,0], [8,1])
+        R = intersection(L1, L2)
+        """
+
+        return area
+
+    def gaussmfAreaU(x, params1, params2):
+        assert len(params1) == 2, 'sigma,c parameter must have exactly two \
+                                   elements.'
+        assert len(params2) == 2, 'sigma,c parameter must have exactly two \
+                                   elements.'
+        s1, m1 = np.r_[params1]     # Zero-indexing in Python
+        s2, m2 = np.r_[params2]     # Zero-indexing in Python
+
+        # Solving coeffiecients of quadratic poly
+        def solve(m1,m2,std1,std2):
+            a = 1/(2*std1**2) - 1/(2*std2**2)
+            b = m2/(std2**2) - m1/(std1**2)
+            c = m1**2 /(2*std1**2) - m2**2 / (2*std2**2) - np.log(std2/std1)
+            return np.roots([a,b,c])
+
+        result = solve(m1,m2,s1,s2)
+        if len(result)==0: # Completely non-overlapping
+            overlap = 0.0
+        if len(result)>0: # One point of contact
+            r = result[0]
+
+            # Calculating area using Cdf
+            area = 1 - norm.cdf(r, m1, s1) + norm.cdf(r,m2,s2)
+            return area
+
+    def trapmfAreaU(x, params1, params2):
+        assert len(params1) == 4, 'a1,b1,c1,d1 parameter must have exactly four\
+                                   elements'
+        assert len(params2) == 4, 'a2,b2,c2,d2 parameter must have exactly four\
+                                   elements'
+        a1, b1, c1, d1 = np.r_[params1]
+        a2, b2, c2, d2 = np.r_[params2]
+
+        py = (a2 - d1)/(c1 -d1 -b2 + a2)
+        # Area
+        area = py*(d1 - a2)/2
+        return area
+
+class mfAreaCertainity:
+
+    def trimfAreaC(x, params1,params2,pUnA, nUnA):
+        assert len(params1) == 3, 'abc parameter must have exactly three \
+                                   elements.'
+        assert len(params2) == 3, 'fgh parameter must have exactly three \
+                                   elements.'
+        a, b, c = np.r_[params1]     # Zero-indexing in Python
+        f, g, h = np.r_[params2]     # Zero-indexing in Python
+        assert a <= b and b <= c, 'abc requires the three elements a <= b <= c.'
+        assert f <= g and g <= h, 'fgh requires the three elements a <= b <= c.'
+        area = 0.5*(c-a) - pUnA - nUnA
+        return area
+
+    def gaussmfAreaC(x, params1, params2, pUnA, nUnA):
+        assert len(params1) == 2, 'sigma,c parameter must have exactly two \
+                                   elements.'
+        assert len(params2) == 2, 'sigma,c parameter must have exactly two \
+                                   elements.'
+        m1, s1 = np.r_[params1]     # Zero-indexing in Python
+        m2, s2 = np.r_[params2]     # Zero-indexing in Python
+
+        if len(x)>1100 :
+            area = 1 - norm.cdf(0, m1, s1) - nUnA - pUnA
+            return area
+        else :
+            area = 1 - pUnA - nUnA
+            return area
+
+    def trapmfAreaC(x, params1, params2, pUnA, nUnA):
+        assert len(params1) == 4, 'a1,b1,c1,d1 parameter must have exactly four\
+                                   elements'
+        assert len(params2) == 4, 'a2,b2,c2,d2 parameter must have exactly four\
+                                   elements'
+        a1, b1, c1, d1 = np.r_[params1]
+        a2, b2, c2, d2 = np.r_[params2]
+
+        # Area
+        totAr = (c1 - b1 + d1 - a1)/2
+        area = totAr -pUnA -nUnA
+        return area
