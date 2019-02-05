5ff03e135deb810e30e2c0464a09b78cd2ca3d95
Roshan Sivakumar <roshan.sivakumar001@gmail.com>
2019-02-05 21:29:22 +0530

    TODO and desc updated.

--- fuzzR.py
+++ fuzzR.py
@@ -1,3 +1,29 @@
+"""
+FuzR
+------
+FuzR is an approach for efficient and optimal deisgn of fuzzy systems without the need of expert knowledge or an
+an expert to know how to automatically update the parameters or choose the membership functions for the
+system. This approach utilizes the filter approach which less computationally intensive compared to the
+wrapper approach to modify or self adapt the parameter by using evolutionary algorithms or neural networks
+
+FuzR is the ratio between the area of certainity and the area of uncertainty
+FuzR only works on datasets in the range 0-10, hence for any datasets with a much larger range, min-max
+normalization must be applied to bring it to that range.
+
+Below, this code represents the FuzR factor for gaussian, trapezoidal and triangular membership functions
+for a given predefined set of intervals with the lower and upper limit.
+The factor was determined for two distributions namely:
+1. Distribution 1 -  [0-100]
+Since FuzR works only in the range of 0-1o, using min-max normalization it was brought into the range
+and the factor wa calculated
+2. Distribution 2 - [0-10]
+FuzR factor was calculated directly for this distribution as it is already in the required range
+
+TODO
+1. The code needs to become more reconfigurable to take inputs as parameters and modify the code throughout
+using variables rather than manually calculating the values for a predefined distribution
+2. Analysis
+"""
 from mfArea import mfAreaCertainity as mfAC
 from mfArea import mfAreaUncertainity as mfAU
 import numpy as np
@@ -5,7 +31,6 @@ import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import pandas as pd
 from scipy.stats import norm
-# data = pd.read_excel("data.xlsx")
 """
 Distribution 1 [0-100]
 --------------
@@ -40,7 +65,8 @@ mft2d2 = fuzz.trimf(dist2, [0.5, 1.85, 3.2])
 mft3d2 = fuzz.trimf(dist2, [2.4, 4.2, 6.0])
 mft4d2 = fuzz.trimf(dist2, [5.0, 7.5, 10.0])
 
-fig, (ax0, ax1, ax2, ax3, ax4, ax5) = plt.subplots(nrows=6, figsize=(7, 10))
+fig, (ax0, ax1, ax2, ax3, ax4, ax5) = plt.subplots(nrows=6, figsize=(7, 9))
+
 ax0.plot(dist1, mft1d1, 'b', linewidth=1)
 ax0.plot(dist1, mft2d1, 'b', linewidth=1)
 ax0.plot(dist1, mft3d1, 'b', linewidth=1)
@@ -153,8 +179,8 @@ Interval4 |    5        |    10       |
 """
 
 # ***********************GAUSSIAN MF'S - Distribution 1*****************************
-mfg1d1 =fuzz.gaussmf(dist1, 14, 65.3)
-mfg2d1 = fuzz.gaussmf(dist1, 33, 16.3)
+mfg1d1 = fuzz.gaussmf(dist1, 14, 4.66)  # 65.3
+mfg2d1 = fuzz.gaussmf(dist1, 33, 2.33)  # 16.3
 mfg3d1 = fuzz.gaussmf(dist1, 44, 21.3)
 mfg4d1 = fuzz.gaussmf(dist1, 57.5, 24.1)
 mfg5d1 = fuzz.gaussmf(dist1, 78.5, 36.7)
@@ -165,9 +191,9 @@ mfg2d2 = fuzz.gaussmf(dist2, 1.85, 0.6)
 mfg3d2 = fuzz.gaussmf(dist2, 4.2, 1.08)
 mfg4d2 = fuzz.gaussmf(dist2, 7.5, 2.08)
 
-#ax2.plot(dist1, mfg1d1, 'r', linewidth=1)
-#ax2.plot(dist1, mfg2d1, 'r', linewidth=1)
-#ax2.plot(dist1, mfg3d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg1d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg2d1, 'r', linewidth=1)
+ax2.plot(dist1, mfg3d1, 'r', linewidth=1)
 ax2.plot(dist1, mfg4d1, 'r', linewidth=1)
 ax2.plot(dist1, mfg5d1, 'r', linewidth=1)
 
@@ -175,9 +201,10 @@ ax3.plot(dist2, mfg1d2, 'r', linewidth=1)
 ax3.plot(dist2, mfg2d2, 'r', linewidth=1)
 ax3.plot(dist2, mfg3d2, 'r', linewidth=1)
 ax3.plot(dist2, mfg4d2, 'r', linewidth=1)
-"""
+
 # Area of Uncertainity - Dist1
 a1gUd1 = mfAU.gaussmfAreaU(dist1, [14, 65.3], [33, 16.3])
+"""
 a2gUd1 = mfAU.gaussmfAreaU(dist1, [33, 16.3], [44, 21.3])
 a3gUd1 = mfAU.gaussmfAreaU(dist1, [44, 21.3], [57.5, 24.1])
 a4gUd1 = mfAU.gaussmfAreaU(dist1, [57.5, 24.1], [78.5, 36.7])
@@ -259,7 +286,7 @@ print("\nfactor for GAUSSIAN MEMBERSHIP FUNCTION- Distribution 2")
 print(totAreagCd2/totAreagUd2)
 
 # Show plot
-#plt.tight_layout()
+# plt.tight_layout()
 
 # ***********************TRAPEZOIDAL MF'S*****************************
 """
@@ -340,9 +367,12 @@ print(totAreatpUd2)
 
 # Area of Certainity - Dist1
 a1tpCd1 = mfAC.trapmfAreaC(dist1, [0.0, 7.0, 21.0, 28.0], [26.0, 29.5, 36.5, 40.0], 0, a1tpUd1)
-a2tpCd1 = mfAC.trapmfAreaC(dist1, [26.0, 29.5, 36.5, 40.0], [36.0, 40.0, 48.0, 52.0], a1tpUd1, a2tpUd1)
-a3tpCd1 = mfAC.trapmfAreaC(dist1, [36.0, 40.0, 48.0, 52.0], [44.0, 50.7, 64.1, 71.0], a2tpUd1, a3tpUd1)
-a4tpCd1 = mfAC.trapmfAreaC(dist1, [44.0, 50.7, 64.1, 71.0], [63.0, 70.7, 83.1, 94.0], a3tpUd1, a4tpUd1)
+a2tpCd1 = mfAC.trapmfAreaC(dist1, [26.0, 29.5, 36.5, 40.0], [
+                           36.0, 40.0, 48.0, 52.0], a1tpUd1, a2tpUd1)
+a3tpCd1 = mfAC.trapmfAreaC(dist1, [36.0, 40.0, 48.0, 52.0], [
+                           44.0, 50.7, 64.1, 71.0], a2tpUd1, a3tpUd1)
+a4tpCd1 = mfAC.trapmfAreaC(dist1, [44.0, 50.7, 64.1, 71.0], [
+                           63.0, 70.7, 83.1, 94.0], a3tpUd1, a4tpUd1)
 a5tpCd1 = mfAC.trapmfAreaC(dist1, [63.0, 70.7, 83.1, 94.0], [0, 0, 0, 0], a4tpUd1, 0)
 
 # Area of Certainity - Dist2
@@ -384,4 +414,27 @@ print(totAreatpCd1/totAreatpUd1)
 print("\nfactor for TRAPEZOIDAL MEMBERSHIP FUNCTION- Distribution 2")
 print(totAreatpCd2/totAreatpUd2)
 
-plt.show()
+
+"""
+# Test & Validation
+
+t1 = fuzz.trimf(dist2, [0.0, 2.0, 4.0])
+t2 = fuzz.trimf(dist2, [2.0, 4.0, 6.0])
+t3 = fuzz.trimf(dist2, [3.0, 6.0, 9.0])
+t4 = fuzz.trimf(dist2, [6.0, 8.0, 10.0])
+
+g1 = fuzz.gaussmf(dist2, 2, 0.75)
+g2 = fuzz.gaussmf(dist2, 4, 0.75)
+g3 = fuzz.gaussmf(dist2, 6, 1.5)
+g4 = fuzz.gaussmf(dist2, 8, 0.75)
+
+ax2.plot(dist2, t1, 'g', linewidth=1)
+ax2.plot(dist2, t2, 'g', linewidth=1)
+ax2.plot(dist2, t3, 'g', linewidth=1)
+ax2.plot(dist2, t4, 'g', linewidth=1)
+
+ax3.plot(dist2, g1, 'r', linewidth=1)
+ax3.plot(dist2, g2, 'r', linewidth=1)
+ax3.plot(dist2, g3, 'r', linewidth=1)
+ax3.plot(dist2, g4, 'r', linewidth=1)
+"""
