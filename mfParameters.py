33908c8d46f5db31a9629cd8710dc5132a79b33c
Roshan Sivakumar <roshan.sivakumar001@gmail.com>
2019-03-20 15:54:47 +0545

    parameters, need to update trap

--- mfParameters.py
+++ mfParameters.py
@@ -16,35 +16,30 @@ class parameters:
         Checking if the range of the distribution os greater than 0-10
         If it is it will normalize it to 0-10
         """
-        #print("Original Interval")
-        #print(self.intervals)
         intervalsMod = []
         flag = 0
         l = len(self.intervals)
         for i in range(l):
             for j in range(2):
                 if (self.intervals[i][j] > 10 or self.intervals[i][j] < 0):
-                    intervalsMod = self.minmaxNorm()
+                    intervalsMod = self._minmaxNorm()
                     flag = 1
                     break
-        #print(self.intervals)
         if(flag == 0):
             return self.intervals
         else:
-            #print("Modified Interval")
-            #print(intervalsMod)
             return intervalsMod
 
 
-    def minmaxNorm(self):
+    def _minmaxNorm(self):
+        """
+        min max normalization to bring intervals into 0-10 range.
+        """
         l = len(self.intervals)
         min = 0
         max = 0
         min = np.min(np.min(self.intervals))
         max = np.max(np.max(self.intervals))
-        print("Min and Max")
-        print(min)
-        print(max)
         for i in range(l):
             for j in range(2):
                 self.intervals[i][j] = ((self.intervals[i][j] - min)/(max-min))*10
@@ -52,6 +47,10 @@ class parameters:
 
 
     def triParam(self):
+        """
+        Function that accepts the intervals declared by the user and returns the
+        parameters - a, b, c for the triangular mrmbership function
+        """
         param = []
         l = len(self.intervals)
         for i in range(l):
@@ -62,6 +61,10 @@ class parameters:
         return param
 
     def trapParam(self):
+        """
+        Function that accepts the intervals declared by the user and returns the
+        parameters - a, b, c, d for the trapezoidal membership function
+        """
         param = []
         l = len(self.intervals)
         for i in range(l):
@@ -73,6 +76,10 @@ class parameters:
         return param
 
     def gaussParam(self):
+        """
+        Function that accepts the intervals declared by the user and returns the
+        parameters - mean and sigma for the Gaussian membership function
+        """
         param = []
         l = len(self.intervals)
         for i in range(l):
